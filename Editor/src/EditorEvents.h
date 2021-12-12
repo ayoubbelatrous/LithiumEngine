e753d896696b5c3e8903f2403939d56a6c31af84
ayoub belatrous <ayoubaelectro@gmail.com>
2021-12-12 19:29:38 +0100

    added editor events

---
+++ Editor/src/EditorEvents.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Event/Event.h"
+namespace Lithium
+{
+	class EditorEvent : public Event
+	{
+
+	};
+
+	class AssetBrowserEvent : public EditorEvent
+	{
+	public:
+		AssetBrowserEvent() = default;
+		const char* GetName() const override {
+
+			return "AssetBrowserEvent";
+		}
+
+		EVENT_TYPE(AssetBrowser);
+	};
+}
