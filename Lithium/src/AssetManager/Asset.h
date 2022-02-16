08e7790a04e714dc4774558d75926f67de0ecdff
ayoubbelatrous <77838146+ayoubbelatrous@users.noreply.github.com>
2022-02-16 15:02:43 -0800

    added asset handle class

--- Lithium/src/AssetManager/Asset.h
+++ Lithium/src/AssetManager/Asset.h
@@ -1,12 +1,43 @@
 #pragma once
-#include "Core/Base.h"
-#include "Renderer/Texture.h"
 
 
 namespace Lithium
 {
-	class Asset
+	template<typename T>
+	class Handle
 	{
+		T _Asset;
 	public:
+		Handle(const std::string& path)
+			:Path(path)
+		{}
+		Handle() = default;
+		~Handle()
+		{
+			*ref -= 1;
+			deleteCallBack();
+		}
+		
+		std::string Path;
+		uint32_t Id;
+		
+		T& GetAsset()
+		{
+			return _Asset;
+		}
+
+		void SetAsset(const T& asset)
+		{
+			_Asset = asset;
+		}
+		
+		uint32_t* ref;
+
+		std::function<void()> deleteCallBack;
+		void SetDeleteCallBack(std::function<void()> callback)
+		{
+			deleteCallBack = callback;
+		}
 	};
 }
+
