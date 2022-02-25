2474de1029bbea15b2b36315f20edf3bc5e8bfc5
ayoubbelatrous <77838146+ayoubbelatrous@users.noreply.github.com>
2022-02-25 11:31:24 +0100

    added uuid class

---
+++ Lithium/src/Core/UUID.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <xhash>
+
+namespace Lithium {
+
+	class UUID
+	{
+	public:
+		UUID();
+		UUID(uint64_t uuid);
+		UUID(const UUID&) = default;
+
+		operator uint64_t() const { return m_UUID; }
+	private:
+		uint64_t m_UUID;
+	};
+
+}
+
+namespace std {
+
+	template<>
+	struct hash<Lithium::UUID>
+	{
+		std::size_t operator()(const Lithium::UUID& uuid) const
+		{
+			return hash<uint64_t>()((uint64_t)uuid);
+		}
+	};
+
+}
