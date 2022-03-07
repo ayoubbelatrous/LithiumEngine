41a20b5b9553de7979497ed33f37540cd9851c3f
ayoubbelatrous <77838146+ayoubbelatrous@users.noreply.github.com>
2022-03-07 11:58:28 +0100

    added script method

---
+++ Lithium/src/Script/ScriptMethod.cpp
@@ -0,0 +1,23 @@
+#include "lipch.h"
+#include "Script/ScriptMethod.h"
+#include "Core/Application.h"
+
+namespace Lithium
+{
+
+	ScriptMethod::ScriptMethod(MonoMethod* m_Method, MonoObject* m_Object)
+		:m_MonoMethod(m_Method),m_MonoObject(m_Object)
+	{}
+
+	void* ScriptMethod::Invoke(void* params)
+	{
+		MonoObject* exception = nullptr;
+		mono_runtime_invoke(m_MonoMethod, m_MonoObject, &params, &exception);
+		if (exception != nullptr)
+		{
+			Application::Get().Monoserver->ForwardMonoException(exception);
+		}
+
+	}
+
+}
---
+++ Lithium/src/Script/ScriptMethod.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <mono/jit/jit.h>
+#include <mono/metadata/assembly.h>
+#include <mono/metadata/object.h>
+#include <mono/metadata/debug-helpers.h>
+#include <mono/metadata/environment.h>
+#include <mono/metadata/attrdefs.h>
+
+namespace Lithium
+{
+	class ScriptMethod
+	{
+	public:
+		ScriptMethod(MonoMethod* m_Method,MonoObject* m_Object);
+
+		//TODO: add a param list class and use it for args and return values
+		void* Invoke(void* params);
+	private:
+		MonoMethod* m_MonoMethod = nullptr;
+		MonoObject* m_MonoObject = nullptr;
+	};
+}
