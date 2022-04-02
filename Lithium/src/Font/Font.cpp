7e96d580e063b8fab01331eb195ed3a6b3d19d17
ayoubbelatrous <77838146+ayoubbelatrous@users.noreply.github.com>
2022-04-02 01:02:27 +0100

    added font class and font atlas generation

--- Lithium/src/Font/Font.cpp
+++ Lithium/src/Font/Font.cpp
@@ -1,92 +1,103 @@
 ﻿#include "lipch.h"
 #include "Font.h"
 #include <msdf-atlas-gen/msdf-atlas-gen.h>
+#include "Core/Base.h"
+
 
 using namespace msdf_atlas;
 
 
-std::tuple<unsigned char*,int> Lithium::Font::GenFonts(const char* fontFilename) {
-	unsigned char* data;
-	bool success = false;
-	// Initialize instance of FreeType library
-	if (msdfgen::FreetypeHandle* ft = msdfgen::initializeFreetype()) {
-		// Load font file
-		if (msdfgen::FontHandle* font = msdfgen::loadFont(ft, fontFilename)) {
-			// Storage for glyph geometry and their coordinates in the atlas
-			
-			std::vector<GlyphGeometry> glyphs;
-			// FontGeometry is a helper class that loads a set of glyphs from a single font.
-			// It can also be used to get additional font metrics, kerning information, etc.
-			FontGeometry fontGeometry(&glyphs);
-			// Load a set of character glyphs:
-			// The second argument can be ignored unless you mix different font sizes in one atlas.
-			// In the last argument, you can specify a charset other than ASCII.
-			// To load specific glyph indices, use loadGlyphs instead.
-			Charset set = Charset();
-			set.add('A');
-			set.add('B');
-			
-			fontGeometry.loadCharset(font, 1.0, set);
-			// Apply MSDF edge coloring. See edge-coloring.h for other coloring strategies.
-			const double maxCornerAngle = 2.0;
-			for (GlyphGeometry& glyph : glyphs)
-				glyph.edgeColoring(&msdfgen::edgeColoringInkTrap, maxCornerAngle, 0);
-			// TightAtlasPacker class computes the layout of the atlas.
-			TightAtlasPacker packer;
-			// Set atlas parameters:
-			// setDimensions or setDimensionsConstraint to find the best value
-			packer.setDimensionsConstraint(TightAtlasPacker::DimensionsConstraint::SQUARE);
-			// setScale for a fixed size or setMinimumScale to use the largest that fits
-			packer.setMinimumScale(24.0);
-			// setPixelRange or setUnitRange
-			packer.setPixelRange(4.0);
-			packer.setMiterLimit(1.0);
-			// Compute atlas layout - pack glyphs
-			packer.pack(glyphs.data(), glyphs.size());
-			// Get final atlas dimensions
-			int width = 0, height = 0;
-			packer.getDimensions(width, height);
-			
-			// The ImmediateAtlasGenerator class facilitates the generation of the atlas bitmap.
-			ImmediateAtlasGenerator<
-				float, // pixel type of buffer for individual glyphs depends on generator function
-				3, // number of atlas color channels
-				&msdfGenerator, // function to generate bitmaps for individual glyphs
-				BitmapAtlasStorage<byte, 3> // class that stores the atlas bitmap
-				// For example, a custom atlas storage class that stores it in VRAM can be used.
-			> generator(width, height);
-			// GeneratorAttributes can be modified to change the generator's default settings.
-			GeneratorAttributes attributes;
-			generator.setAttributes(attributes);
-			generator.setThreadCount(2);
-			// Generate atlas bitmap
-			generator.generate(glyphs.data(), glyphs.size());
-			
-			// The atlas bitmap can now be retrieved via atlasStorage as a BitmapConstRef.
-			// The glyphs array (or fontGeometry) contains positioning data for typesetting text.
-			//success = myProject::submitAtlasBitmapAndLayout(generator.atlasStorage(), glyphs);
-			for (auto g:glyphs)
-			{
-				int x, y, w, h = 0;
-				g.getBoxRect(x, y, w, h);
-				CORE_LOG("codepoint : " << (uint32_t)g.getCodepoint() << "");
-				CORE_LOG("X : " << x);
-				CORE_LOG("Y : " << y);
-				CORE_LOG("W : " << w);
-				CORE_LOG("H : " << h);
-			}
-			
-			
-			msdfgen::savePng(generator.atlasStorage(),"assets/atlas.png");
-			msdfgen::BitmapConstRef<byte, 3> bitmap = generator.atlasStorage();
+namespace Lithium
+{
+	static msdfgen::FreetypeHandle* freeTypeHandle = nullptr;
 
-			std::vector<byte> pixels(3 * bitmap.width * bitmap.height);
-			for (int y = 0; y < bitmap.height; ++y)
-				memcpy(&pixels[3 * bitmap.width * y], bitmap(0, bitmap.height - y - 1), 3 * bitmap.width);
-			return std::make_tuple<unsigned char*,int>(&pixels[0], 3 * bitmap.width * bitmap.height);
-			msdfgen::destroyFont(font);
-		}
-		msdfgen::deinitializeFreetype(ft);
+	Font::Font(const std::string& Filepath)
+		:m_Path(Filepath)
+	{
+		Load();
 	}
+
+	void Font::Init()
+	{
+		freeTypeHandle = msdfgen::initializeFreetype();
+		if (freeTypeHandle == nullptr)
+		{
+			CORE_LOG("[Font] : Failed to init FreeType");
+			ASSERT(freeTypeHandle == nullptr);
+		}
+	}
+
+	Font::Character Font::GetCharacter(uint32_t character)
+	{
+		return m_CharacterMap[character];
+	}
+
+	Ref<Texture> Font::GetAtlas()
+	{
+		return m_AtlasTexture;
+	}
+
+	void Font::Load()
+	{
+		msdfgen::FontHandle* fontHandle = nullptr;
+		fontHandle = msdfgen::loadFont(freeTypeHandle, m_Path.c_str());
+		if (fontHandle == nullptr) {
+			CORE_LOG("[Font] : Failed to load font file");
+			ASSERT(fontHandle == nullptr);
+		}
+
+		std::vector<GlyphGeometry> glyphs;
+		FontGeometry fontGeometry(&glyphs);
+		
+		fontGeometry.loadCharset(fontHandle, 1.0, Charset::ASCII);
+
+		const double maxCornerAngle = 3.0;
+		for (GlyphGeometry& glyph : glyphs)
+			glyph.edgeColoring(&msdfgen::edgeColoringInkTrap, maxCornerAngle, 0);
+
+
+		std::string pCachePath = std::string("cache/") + std::filesystem::path(m_Path + "-cache.png").filename().string();
+
+		TightAtlasPacker packer;
+
+		packer.setDimensionsConstraint(TightAtlasPacker::DimensionsConstraint::SQUARE);
+		
+		packer.setMinimumScale(24.0);
+		packer.setPixelRange(2.0);
+		packer.setMiterLimit(1.0);
+		packer.pack(glyphs.data(), glyphs.size());
+		int width = 0, height = 0;
+		packer.getDimensions(width, height);
+		ImmediateAtlasGenerator<float,3,&msdfGenerator,BitmapAtlasStorage<byte, 3>> generator(width, height);
+		GeneratorAttributes attributes;
+		generator.setAttributes(attributes);
+		generator.setThreadCount(4);
+		generator.generate(glyphs.data(), glyphs.size());
+		msdfgen::BitmapConstRef<byte, 3> bitmap = generator.atlasStorage();
+
+
+
+		msdfgen::savePng(bitmap, pCachePath.c_str());
+
+		m_AtlasHeight = bitmap.height;
+		m_AtlasWidth = bitmap.width;
+
+
+
+		m_AtlasTexture = CreateRef<Texture>(pCachePath);
+
+
+		for (auto glyph : glyphs)
+		{
+			int x, y, w, h = 0;
+			glyph.getBoxRect(x, y, w, h);
+		
+			m_CharacterMap[(uint32_t)glyph.getCodepoint()] = {glm::vec2(w,h),glm::vec2(x,y),(uint32_t)glyph.getCodepoint()};
+
+		}
+
 	
+
+		msdfgen::destroyFont(fontHandle);
+	}
 }
--- Lithium/src/Font/Font.h
+++ Lithium/src/Font/Font.h
@@ -1,7 +1,34 @@
 #pragma once
-#include <tuple>
+#include "Renderer/Texture.h"
 
+namespace Lithium {
+	struct Character {
+		glm::vec2 PackedSize;
+		glm::vec2 PackedPos;
+		uint32_t CodePoint;
+	};
 
-namespace Lithium::Font {
-	std::tuple<unsigned char*,int> GenFonts(const char* fontFilename);
+	class Font
+	{
+		
+	public:
+		struct Character {
+			glm::vec2 PackedSize;
+			glm::vec2 PackedPos;
+			uint32_t CodePoint;
+		};
+		Font() = default;
+		Font(const std::string& Filepath);
+		static void Init();
+
+		Character GetCharacter(uint32_t character);
+		Ref<Texture> GetAtlas();
+	private:
+		void Load();
+		std::string m_Path;
+		std::map<uint32_t, Character> m_CharacterMap;
+		Ref<Texture> m_AtlasTexture;
+		uint32_t m_AtlasWidth = 1;
+		uint32_t m_AtlasHeight = 1;
+	};
 }
